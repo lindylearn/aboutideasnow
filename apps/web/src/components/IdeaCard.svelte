1f09975201e32b1c122a6cccf46a9085f3eb5c60
Peter Hagen <peter@lindylearn.io>
2024-01-11 15:44:44 +0100

    Put now tag after domain field

--- apps/web/src/components/IdeaCard.svelte
+++ apps/web/src/components/IdeaCard.svelte
@@ -14,14 +14,18 @@
             class="w-10 h-10 mr-2"
             src="https://www.google.com/s2/favicons?sz=256&domain_url={post.domain}"
         />
-        <div class="flex flex-col justify-center">
-            <h2 class="font-bold">{post.domain}</h2>
+        <div class="flex flex-row justify-center items-center gap-2">
+            <h2 class="font-bold">
+                {post.domain}
+            </h2>
+            <div class="flex">
+                {#if post.type === "NOW"}
+                    <div class="w-auto px-2 py-1 text-sm text-white bg-gray-300 rounded-md">
+                        /now
+                    </div>
+                {/if}
+            </div>
         </div>
     </div>
     <div class="mb-2 whitespace-pre-wrap line-clamp-6">{post.content}</div>
-    <div class="flex">
-        {#if post.type === "NOW"}
-            <div class="w-auto px-2 py-1 text-sm text-white bg-gray-300 rounded-md">/now</div>
-        {/if}
-    </div>
 </a>
