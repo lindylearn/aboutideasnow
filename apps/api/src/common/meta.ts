69d00d53ae8a66502266a3c642f64b9fc9830e25
Peter Hagen <peter@lindylearn.io>
2024-01-11 18:21:50 +0100

    Disable find-date parsing

--- apps/api/src/common/meta.ts
+++ apps/api/src/common/meta.ts
@@ -2,8 +2,6 @@
 import Parser from "@postlight/parser";
 import _metascraper from "metascraper";
 import _metascraperDate from "metascraper-date";
-import { findDates } from "find-dates";
-import * as chrono from "chrono-node";
 import { openai } from "./openai.js";
 
 // @ts-ignore
@@ -15,15 +13,6 @@ export async function getMeta(url: string, html: string, content?: string) {
 
     // Try using metadata data
     let date = meta.date ? new Date(meta.date) : undefined;
-
-    // Try another extractor library
-    if (!date) {
-        const results = findDates(html) as { match: string }[];
-        if (results.length) {
-            date = chrono.parseDate(results[0].match);
-        }
-    }
-
     // Don't trust current or future dates, e.g. on https://francescasciandra.art/now
     if (date && date.toISOString().slice(0, 10) >= new Date().toISOString().slice(0, 10)) {
         date = undefined;
