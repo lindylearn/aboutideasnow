581c1a2e06cc53fd96385fca4076d9c2af940662
Peter Hagen <peter@lindylearn.io>
2024-01-13 15:01:35 +0100

    Deploy sample api change

--- apps/api/src/routes/hello.ts
+++ apps/api/src/routes/hello.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
 
 export async function hello(req: Request, res: Response) {
-    return res.json({ message: "Hello World!" });
+    return res.json({ message: "Hello World!!!" });
 }
