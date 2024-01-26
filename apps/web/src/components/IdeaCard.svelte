50b1b345762b2a4ed7d55852ed58ee29104583e7
Peter Hagen <peter@lindylearn.io>
2024-01-26 19:01:40 +0100

    Put updated date inside the post text

--- apps/web/src/components/IdeaCard.svelte
+++ apps/web/src/components/IdeaCard.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { updated } from "$app/stores";
     import type { Post } from "@repo/core/generated/prisma-client";
     export let post: Post;
     export let color: string;
@@ -21,7 +22,7 @@
                 <h2 class="mr-0 font-sans font-bold text-slate-700">
                     {post.domain}
                 </h2>
-                <div class="flex">
+                <div class="flex gap-2">
                     {#if post.type === "NOW"}
                         <div class="w-auto px-2 py-1 text-sm rounded-md bg-slate-300">/now</div>
                     {/if}
@@ -34,16 +35,23 @@
             <div class="w-3 h-3 bg-green-400 rounded-full"></div>
         </div>
     </div>
-    <div class="bg-light text-sm px-4 pt-4 whitespace-pre-wrap line-clamp-[10]">
+    <div class="bg-light text-sm px-4 pt-4 whitespace-pre-wrap line-clamp-[16] rounded-b-md">
         <div class="relative">
-            {post.content}
+            {#if post.updatedAt}
+                Updated {new Intl.DateTimeFormat("en-US", {
+                    month: "long",
+                    day: "numeric",
+                    year: "numeric"
+                }).format(post.updatedAt)}
+                <br /><br />{/if}{post.content}
             <div
                 class="absolute bottom-0 w-full h-16 pointer-events-none bg-gradient-to-t from-white via-white via-30%"
             ></div>
         </div>
     </div>
 
-    {#if post.updatedAt && post.updatedAt.getFullYear() !== 1970}
+    <!-- Maybe the updated date looks less distracting inside the post text? -->
+    <!-- {#if post.updatedAt && post.updatedAt.getFullYear() !== 1970}
         <div class="p-2">
             <div class="text-xs font-bold uppercase text-slate-500">Last updated:</div>
             <div class="font-sans font-bold text-slate-700">
@@ -54,5 +62,5 @@
                 }).format(post.updatedAt)}
             </div>
         </div>
-    {/if}
+    {/if} -->
 </a>
