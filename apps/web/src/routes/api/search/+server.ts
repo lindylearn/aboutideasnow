0ed2b070d5d0d10e53adbb193f0e31bdd355d77c
Peter Hagen <peter@lindylearn.io>
2024-01-29 15:29:17 +0100

    Parallelize database search

--- apps/web/src/routes/api/search/+server.ts
+++ apps/web/src/routes/api/search/+server.ts
@@ -10,23 +10,20 @@ export const GET: RequestHandler = async ({ url }) => {
     }
 
     const t0 = Date.now();
-    const embedding = await generateEmbedding(query);
+    const [keywordSearchResponse, semanticSearchResponse] = await Promise.all([
+        supabase.rpc("kw_match_posts", {
+            query_text: query,
+            match_count: 10
+        }),
+        generateEmbedding(query).then((embedding) =>
+            supabase.rpc("match_posts", {
+                query_embedding: embedding,
+                match_count: 10
+            })
+        )
+    ]);
     const t1 = Date.now();
-    console.log(`Embedding generated in ${t1 - t0}ms`);
-
-    // Query the database
-    const semanticSearchResponse = await supabase.rpc("match_posts", {
-        query_embedding: embedding,
-        match_count: 10
-    });
-    const t2 = Date.now();
-    console.log(`Semantic search performed in ${t2 - t1}ms`);
-    const keywordSearchResponse = await supabase.rpc("kw_match_posts", {
-        query_text: embedding,
-        match_count: 10
-    });
-    const t3 = Date.now();
-    console.log(`Keyword search performed in ${t3 - t2}ms`);
+    console.log(`Search completed in ${t1 - t0}ms`);
 
     // Return errors
     if (semanticSearchResponse.error) {
@@ -46,8 +43,6 @@ export const GET: RequestHandler = async ({ url }) => {
         ...keywordSearchResponse.data
     ];
     posts.sort((a, b) => b.similarity - a.similarity);
-
-    console.log(`Search completed in ${Date.now() - t0}ms\n`);
 
     // importing json() helper from @sveltejs/kit causes a build error
     return new Response(JSON.stringify({ posts }));
