ae83faa9cb84f9f0afe91d802df50dc76df6921c
Peter Hagen <peter@lindylearn.io>
2024-01-29 15:00:07 +0100

    Add search time logging

--- apps/web/src/routes/api/search/+server.ts
+++ apps/web/src/routes/api/search/+server.ts
@@ -9,17 +9,24 @@ export const GET: RequestHandler = async ({ url }) => {
         return new Response(JSON.stringify({ error: "No query provided" }), { status: 400 });
     }
 
+    const t0 = Date.now();
     const embedding = await generateEmbedding(query);
+    const t1 = Date.now();
+    console.log(`Embedding generated in ${t1 - t0}ms`);
 
     // Query the database
     const semanticSearchResponse = await supabase.rpc("match_posts", {
         query_embedding: embedding,
         match_count: 10
     });
+    const t2 = Date.now();
+    console.log(`Semantic search performed in ${t2 - t1}ms`);
     const keywordSearchResponse = await supabase.rpc("kw_match_posts", {
         query_text: embedding,
         match_count: 10
     });
+    const t3 = Date.now();
+    console.log(`Keyword search performed in ${t3 - t2}ms`);
 
     // Return errors
     if (semanticSearchResponse.error) {
@@ -39,6 +46,8 @@ export const GET: RequestHandler = async ({ url }) => {
         ...keywordSearchResponse.data
     ];
     posts.sort((a, b) => b.similarity - a.similarity);
+
+    console.log(`Search completed in ${Date.now() - t0}ms\n`);
 
     // importing json() helper from @sveltejs/kit causes a build error
     return new Response(JSON.stringify({ posts }));
