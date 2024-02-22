3d258707ae6e4ca35a9bb966dff57da4057cf395
Louis Barclay <hi@louis.work>
2024-02-22 15:20:17 +0000

    feat: abstract form away

--- apps/web/src/routes/about/+page.svelte
+++ apps/web/src/routes/about/+page.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-    import { enhance } from "$app/forms";
     import type { ActionData } from "./$types";
-    import { colorPalette } from "../../common/constants";
-
-    export let form: ActionData;
-
+    import Form from "../../components/Form.svelte";
     let isAddingDomain = false;
+    export let form: ActionData;
 </script>
 
 <main class="flex flex-col w-full max-w-xl gap-5 text-lg md:gap-10 items-left">
@@ -54,55 +51,7 @@
             page - or all three.
         </p>
         <p>The more you write, the more likely people can find you.</p>
-        <div class="flex flex-col items-center mt-4 text-center">
-            {#if form?.addedDomain === false}
-                Error indexing your domain :(<br />We will take a look and add your site as soon as
-                possible!
-            {:else if form?.scrapedPosts}
-                {#if form?.scrapedPosts.length === 0}
-                    We didn't find a /now, /about, or /ideas page on your website. Add one and try
-                    again!
-                {:else}
-                    Indexed your website successfully! Found posts:
-                    <ul class="list-disc">
-                        {#each form.scrapedPosts as post}
-                            <li>
-                                {post.url}
-                                last updated at {new Intl.DateTimeFormat("en-US", {
-                                    month: "long",
-                                    day: "numeric",
-                                    year: "numeric"
-                                }).format(new Date(post.updatedAt))}
-                            </li>
-                        {/each}
-                    </ul>
-                {/if}
-            {:else if isAddingDomain}
-                Indexing your domain...
-            {:else}
-                <form
-                    class="flex flex-col items-center gap-2 w-96 md:w-full md:gap-4 md:flex-row"
-                    method="POST"
-                    use:enhance={() => {
-                        // Show loading state until page data is reloaded
-                        isAddingDomain = true;
-                    }}
-                >
-                    <input
-                        class="px-3 py-2 text-lg border rounded-lg shadow-lg outline-none grow bg-light placeholder:text-text/30 border-border"
-                        placeholder="mywebsite.com"
-                        name="domain"
-                        required
-                    />
-                    <button
-                        class="px-3 py-2 font-bold text-white border rounded-lg shadow-lg border-border"
-                        style:background-color={colorPalette[0]}
-                    >
-                        Add my site
-                    </button>
-                </form>
-            {/if}
-        </div>
+        <Form {form} {isAddingDomain} />
     </section>
 
     <section>
