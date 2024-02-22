11cc112e72fc23f460e43051b9a228fbbe961cac
Louis Barclay <hi@louis.work>
2024-02-22 15:20:10 +0000

    feat: remove functionality

--- apps/web/src/routes/about/+page.server.ts
+++ apps/web/src/routes/about/+page.server.ts
@@ -1,24 +1,7 @@
-import { INTERNAL_API_URL } from "$env/static/private";
-import type { Post } from "@repo/core/generated/prisma-client";
+import { handleSubmit } from "../../common/formActions.js";
 
 export const actions = {
-    default: async ({ request, fetch }) => {
-        const data = await request.formData();
-        const domain = data.get("domain");
-
-        const response = await fetch(`${INTERNAL_API_URL}/add-domain?url=${domain}`, {
-            method: "POST"
-        });
-        if (!response.ok) {
-            return {
-                addedDomain: false
-            };
-        }
-        const scrapedPosts: Post[] = await response.json();
-
-        return {
-            addedDomain: true,
-            scrapedPosts
-        };
+    default: async ({ request }) => {
+        return await handleSubmit(request);
     }
 };
