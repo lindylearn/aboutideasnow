40d01efcb13901864c481a74a59d626fa4b5536c
Will Holley <will@willholley.com>
2024-02-26 11:46:40 -0500

    fix(apps/web): Lack of database connection will not block rendering

--- apps/web/src/routes/+page.server.ts
+++ apps/web/src/routes/+page.server.ts
@@ -1,41 +1,56 @@
 import { getDatabaseClient } from "@repo/core/dist";
-import type { PostType } from "@repo/core/generated/prisma-client";
+import type { PostType, Post } from "@repo/core/generated/prisma-client";
 import { handleSubmit } from "../common/formActions.js";
 
-export async function load({ url, setHeaders }) {
-    const db = getDatabaseClient();
+export async function load({ url, setHeaders }): Promise<{
+    websiteCount: number;
+    defaultPosts: Post[];
+}> {
+    let websiteCount: number = 0;
+    let defaultPosts: Post[] = [];
 
-    const postTypeFilter = (url.searchParams.get("filter")?.toUpperCase() as PostType) || "IDEAS";
-    const defaultPosts = await db.post.findMany({
-        where: {
-            type: postTypeFilter
-        },
-        orderBy: [
-            {
-                updatedAt: "desc"
+    try {
+        const db = getDatabaseClient();
+
+        const postTypeFilter =
+            (url.searchParams.get("filter")?.toUpperCase() as PostType) || "IDEAS";
+
+        defaultPosts = await db.post.findMany({
+            where: {
+                type: postTypeFilter
             },
-            {
-                domain: "desc"
+            orderBy: [
+                {
+                    updatedAt: "desc"
+                },
+                {
+                    domain: "desc"
+                }
+            ],
+            take: postTypeFilter ? 15 : 9
+        });
+
+        console.log(defaultPosts.length);
+
+        websiteCount = await db.scrapeState.count({
+            where: {
+                status: "SCRAPED",
+                type: "ABOUT"
             }
-        ],
-        take: postTypeFilter ? 15 : 9
-    });
-
-    console.log(defaultPosts.length);
-
-    const websiteCount = await db.scrapeState.count({
-        where: {
-            status: "SCRAPED",
-            type: "ABOUT"
-        }
-    });
+        });
+    } catch (error) {
+        console.error(error);
+    }
 
     // Cache for 1 hour
     setHeaders({
         "Cache-Control": "max-age=0, s-max-age=3600"
     });
 
-    return { websiteCount, defaultPosts };
+    return {
+        websiteCount,
+        defaultPosts
+    };
 }
 
 export const actions = {
