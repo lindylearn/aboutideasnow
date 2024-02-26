5acb50a7d96d02e04bedb6ba0df38ba704ded383
Peter Hagen <peter@lindylearn.io>
2024-02-26 17:16:48 +0100

    Add error fallback

--- apps/web/src/routes/+page.server.ts
+++ apps/web/src/routes/+page.server.ts
@@ -5,37 +5,42 @@ import { handleSubmit } from "../common/formActions.js";
 export async function load({ url, setHeaders }) {
     const db = getDatabaseClient();
 
-    const postTypeFilter = (url.searchParams.get("filter")?.toUpperCase() as PostType) || "IDEAS";
-    const defaultPosts = await db.post.findMany({
-        where: {
-            type: postTypeFilter
-        },
-        orderBy: [
-            {
-                updatedAt: "desc"
+    try {
+        const postTypeFilter =
+            (url.searchParams.get("filter")?.toUpperCase() as PostType) || "IDEAS";
+        const defaultPosts = await db.post.findMany({
+            where: {
+                type: postTypeFilter
             },
-            {
-                domain: "desc"
+            orderBy: [
+                {
+                    updatedAt: "desc"
+                },
+                {
+                    domain: "desc"
+                }
+            ],
+            take: postTypeFilter ? 15 : 9
+        });
+
+        console.log(defaultPosts.length);
+
+        const websiteCount = await db.scrapeState.count({
+            where: {
+                status: "SCRAPED",
+                type: "ABOUT"
             }
-        ],
-        take: postTypeFilter ? 15 : 9
-    });
+        });
 
-    console.log(defaultPosts.length);
+        // Cache for 1 hour
+        setHeaders({
+            "Cache-Control": "max-age=0, s-max-age=3600"
+        });
 
-    const websiteCount = await db.scrapeState.count({
-        where: {
-            status: "SCRAPED",
-            type: "ABOUT"
-        }
-    });
-
-    // Cache for 1 hour
-    setHeaders({
-        "Cache-Control": "max-age=0, s-max-age=3600"
-    });
-
-    return { websiteCount, defaultPosts };
+        return { websiteCount, defaultPosts };
+    } catch {
+        return { websiteCount: 7400, defaultPosts: [] };
+    }
 }
 
 export const actions = {
