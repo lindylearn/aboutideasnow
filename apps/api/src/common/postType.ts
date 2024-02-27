da3c289b7cbe2b7b88cb940985e11db782a77b53
Peter Hagen <peter@lindylearn.io>
2024-02-27 11:12:11 +0100

    Fix jakeseliger.com

--- apps/api/src/common/postType.ts
+++ apps/api/src/common/postType.ts
@@ -1,6 +1,11 @@
 import { PostType } from "@repo/core/generated/prisma-client";
 
 export function getPostType(pathname: string) {
+    // Ignore trailing slash
+    if (pathname.endsWith("/")) {
+        pathname = pathname.slice(0, -1);
+    }
+
     // Treat / as /about
     if (pathname === "/") {
         pathname = "/about";
